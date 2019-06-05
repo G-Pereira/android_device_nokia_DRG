68c33fe4b53047f8d858d070ec376a3b2da3aa76
Gonçalo Pereira <goncalo_pereira@outlook.pt>
2019-06-05 23:23:53 +0100

    Updated to new target style definitions

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -21,7 +21,42 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_DRG.mk \
     $(LOCAL_DIR)/mk_DRG.mk \
     $(LOCAL_DIR)/nitrogen_DRG.mk \
-    $(LOCAL_DIR)/du.mk \
+    $(LOCAL_DIR)/du_DRG.mk \
     $(LOCAL_DIR)/rr_DRG.mk \
     $(LOCAL_DIR)/syberia_DRG.mk \
     $(LOCAL_DIR)/viper_DRG.mk \
+
+COMMON_LUNCH_CHOICES := \
+	aosp_DRG-eng \
+	aosp_DRG-userdebug \
+	aosp_DRG-user \
+	dot_DRG-eng \
+	dot_DRG-userdebug \
+	dot_DRG-user \
+	havoc_DRG-eng \
+	havoc_DRG-userdebug \
+	havoc_DRG-user \
+	lineage_DRG-eng \
+	lineage_DRG-userdebug \
+	lineage_DRG-user \
+	mk_DRG-eng \
+	mk_DRG-userdebug \
+	mk_DRG-user \
+	nitrogen_DRG-eng \
+	nitrogen_DRG-userdebug \
+	nitrogen_DRG-user \
+	rr_DRG-eng \
+	rr_DRG-userdebug \
+	rr_DRG-user \
+	syberia_DRG-eng \
+	syberia_DRG-userdebug \
+	syberia_DRG-user \
+	superior_DRG-eng \
+	superior_DRG-userdebug \
+	superior_DRG-user \
+	du_DRG-eng \
+	du_DRG-userdebug \
+	du_DRG-user \
+	viper_DRG-eng \
+	viper_DRG-userdebug \
+	viper_DRG-user
